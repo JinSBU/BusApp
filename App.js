9881e0d79fb70617e90b1881d91d26a5bfc9d057
Jin <jin.zhen@stonybrook.edu>
2019-01-04 17:25:26 -0500

    Need to add in bus arrival times

--- App.js
+++ App.js
@@ -6,8 +6,7 @@ import busStopImage from './assets/BusStop.png'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import busStops from './constants/BusStops.js'
 // import axios from 'axios'
-
-console.log(busStops.stopName);
+let busText = "";
 export default class App extends React.Component {
   constructor(props){
      super(props);
@@ -21,45 +20,30 @@ export default class App extends React.Component {
          latitudeDelta: 0.01622,
          longitudeDelta: 0.017021
        },
-       markers: [
-      {
-         title: 'Hello',
-         description: 'here is the description',
-         coordinates: {
-           latitude: 40.912959,
-           longitude: -73.129656
-         },
-       },
-       {
-         title: 'Bus stop 2',
-         description: 'here is the description',
-         coordinates: {
-           latitude: 40.913959,
-           longitude: -73.120656
-         },
-       }]
      }
 
   }
-  // componentWillMount(){
-  //   // fetch("https://smarttransit.cewit.stonybrook.edu:8443/buses")
-  //   axios.get('https://smarttransit.cewit.stonybrook.edu:8443/buses')
-  //     .then((response1) =>
-  //       this.setState({ bus: response1.data}));
-  //   axios.get('https://smarttransit.cewit.stonybrook.edu:8443/stops')
-  //     .then((response2) =>
-  //       this.setState({ stopData: response2.data}));
-  // }
+  fetchData(i){    
+    fetch('https://smarttransit.cewit.stonybrook.edu:8443/stops/'+ i.toString() + '/buses')
+    .then((response) => response.json())
+    .then((responseJson) => {
+        console.log(responseJson);
+        let busText = responseJson;
+    })
+  }
   renderMarkers(){
+    var baseLink = "https://smarttransit.cewit.stonybrook.edu:8443";
     return busStops.stops.map((stop, i) => {
+      <Text></Text>
       return <Marker
         key={ i }
         
         coordinate={{latitude: stop.location.latitude, longitude: stop.location.longitude}}
-        onClick = { this.onMarkerClick }
+        onPress={ () => this.fetchData(stop.stopID) }
         title = {stop.stopName}
         image = {busStopImage}
-        name = { stop.stopName } />
+        description = {busText} />
+        
     })
   }
   render() {
